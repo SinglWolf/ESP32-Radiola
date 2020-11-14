4bb2e77d32f01c2ac2f46b202c745a360b60d98f
SinglWolf <32172969+SinglWolf@users.noreply.github.com>
2020-11-14 13:15:27 +0500

    Ver. 2.1 Rev. 1

--- include/main.h
+++ include/main.h
@@ -67,7 +67,7 @@
 #include "driver/timer.h"
 
 #define RELEASE "2.1"
-#define REVISION "0"
+#define REVISION "1"
 
 #define TIMER_DIVIDER 16                        //5000000Hz 5MHz
 #define TIMER_DIVIDER1MS TIMER_BASE_CLK / 10000 //10000Hz
